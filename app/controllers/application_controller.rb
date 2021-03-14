9ad19b5f416bce78760ea697a4967c2018b0d8bc
Vitalie D <vitalie.dev@gmail.com>
2021-03-14 20:48:02 +0200

    User auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::API
   include ExceptionHandling
+
+  before_action :authorize_user!
+
+  def current_user
+    @current_user
+  end
+
+  private
+
+  def authorize_user!
+    token = request.headers[:Authorization]
+    @current_user = User.find_by_token(token)
+    raise Authentication::Unauthorized if @current_user.blank?
+  end
 end
